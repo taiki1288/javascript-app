7b824097a23a7881a7bca3ded05cb674d5d9e5e0
taiki yamashita <taiki0808ty@gmail.com>
2021-05-23 22:16:05 +0900

    apiを返す。

---
+++ index.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("addBtn");
+const lists = document.getElementById("lists");
+
+button.addEventListener("click", async function () {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    console.log(users);
+})
