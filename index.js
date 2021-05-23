6872c824e09ad0957a7a8a6590ab2c1325cb1d6e
taiki yamashita <taiki0808ty@gmail.com>
2021-05-23 22:21:16 +0900

    listに要素を追加

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const button = document.getElementById("addBtn");
 const lists = document.getElementById("lists");
 
 button.addEventListener("click", async function () {
+    // データのやりとり
     const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await res.json();
-    console.log(users);
+
+    //DOM操作
+    const list = document.createElement("li");
+    list.innerText = "foo";
+    lists.appendChild(list);
+    console.log(list);
 })
